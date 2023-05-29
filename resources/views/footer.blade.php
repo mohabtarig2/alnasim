d9b6a8e89feb2e03b6a549746c320516eb6d078f
mohabtarig2 <mohab.tarig@gmail.com>
2023-05-29 13:31:32 +0400

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,21 @@
+
+    <script src="{{ URL::asset("assets/js/jquery-2.2.4.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/bootstrap.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/counterup.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/imagesloaded.pkgd.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/isotope.pkgd.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/jquery.appear.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/jquery.meanmenu.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/jquery.nice-select.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/jquery.scrollUp.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/lightcase.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/odometer.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/owl.carousel.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/sticky-header.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/wow.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/tilt.jquery.min.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/waypoint.js")}}"></script>
+    <script src="{{ URL::asset("assets/js/main.js")}}"></script>
+
+
+
---
+++ resources/views/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+
+
+
+
+<head>
+
+    <!--========= Required meta tags =========-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>AL NASEEM ALALEEL - AIR CONDITION REPAIRING AND MAINTENANCE</title>
+
+    <!--====== Favicon ======-->
+    
+
+    <!--====== CSS Here ======-->
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/bootstrap.min.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/animate.min.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/font-awesome.min.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/lightcase.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/meanmenu.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/nice-select.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/owl.carousel.min.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/odometer.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/default.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/style.css")}}'>
+    <link rel="stylesheet"  href='{{ URL::asset("assets/css/responsive.css")}}'>
+
+</head>
